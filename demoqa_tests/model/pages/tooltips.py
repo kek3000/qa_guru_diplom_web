0298bac3119a41447ee546416a9aa943db5e4ff2
Nikit Alekseev <n.alekseev@comagic.dev>
2023-07-21 14:39:36 +0300

    diplom web fix

--- demoqa_tests/model/pages/tooltips.py
+++ demoqa_tests/model/pages/tooltips.py
@@ -14,7 +14,7 @@ class TooltipPage:
         return self
 
     def assert_text_field(self):
-        browser.element('div.tooltip').should(have.text('You hovered over the text field'))
+        browser.element('#textFieldToolTip').should(have.text('You hovered over the text field'))
         return self
 
     def set_focus_in_button(self):
