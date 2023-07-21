ce0e496d19905f5f6539f85f9f33d0c5c2145a97
Nikit Alekseev <n.alekseev@comagic.dev>
2023-07-21 14:27:11 +0300

    diplom web fix

--- tests/test_tooltips.py
+++ tests/test_tooltips.py
@@ -6,7 +6,7 @@ tooltips = TooltipPage()
 
 @allure.label('owner', 'Nikita Alekseev')
 @allure.title('Text field tooltip')
-def test_confirm_alert():
+def test_confirm_alert_field():
     with allure.step('Opening the hint page'):
         tooltips.open()
     with allure.step('Setting focus on the field'):
@@ -14,9 +14,10 @@ def test_confirm_alert():
     with allure.step('Check hint text'):
         tooltips.assert_text_field()
 
+
 @allure.label('owner', 'Nikita Alekseev')
 @allure.title('Text button tooltip')
-def test_confirm_alert():
+def test_confirm_alert_button():
     with allure.step('Opening the hint page'):
         tooltips.open()
     with allure.step('Setting focus on the button'):
